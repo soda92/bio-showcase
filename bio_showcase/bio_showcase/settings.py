4f1ce1ba116434c2052f2d0c18879fdc7ef999a7
Maple <wjxa20152015@gmail.com>
2025-04-18 02:46:47 +0800

    remove secret key

--- bio_showcase/bio_showcase/settings.py
+++ bio_showcase/bio_showcase/settings.py
@@ -10,6 +10,7 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/5.2/ref/settings/
 """
 
+import os
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -20,10 +21,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-k60vzg3)e0+0(gjh*r)#de$m5icpw7aaafdj*yr=i13$o%j4)5'
+SECRET_KEY = os.getenv("DJANGO_SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False if os.getenv("DJANGO_PRODUCTION") else True
 
 ALLOWED_HOSTS = []
 
