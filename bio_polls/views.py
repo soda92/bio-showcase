1979e6ccb3763128dc5b43ed56ccb6c7888811ed
Maple <wjxa20152015@gmail.com>
2025-05-13 08:23:26 +0800

    add voting

--- bio_polls/views.py
+++ bio_polls/views.py
@@ -1,7 +1,9 @@
-from django.http import HttpResponse
-from .models import Question
+from django.http import HttpResponse, HttpResponseRedirect
+import django.urls
+from .models import Question, Choice
 import django.shortcuts
 import django.http
+import django.db.models
 
 
 def index(request):
@@ -23,9 +25,33 @@ def detail(request, question_id):
 
 
 def results(request, question_id):
-    response = "You're looking at the results of question %s."
-    return HttpResponse(response % question_id)
+    question = django.shortcuts.get_object_or_404(Question, pk=question_id)
+    return django.shortcuts.render(
+        request=request,
+        template_name='polls/results.html',
+        context={'question': question},
+    )
 
 
 def vote(request, question_id):
-    return HttpResponse("You're voting on question %s." % question_id)
+    question: Question = django.shortcuts.get_object_or_404(Question, pk=question_id)
+    try:
+        selected_choice: Choice = question.choice_set.get(pk=request.POST['choice'])
+    except (KeyError, Choice.DoesNotExist):
+        # Redisplay the question voting form.
+        return django.shortcuts.render(
+            request=request,
+            template_name='polls/detail.html',
+            context={
+                'question': question,
+                'error_message': "You didn't select a choice.",
+            },
+        )
+    else:
+        selected_choice.votes = django.db.models.F('votes') + 1
+        selected_choice.save()
+        # Always return an HttpResponseRedirect after sucessfully dealing with POST data.
+        # This prevents data from being posted twice if a use hits the Back button.
+        return HttpResponseRedirect(
+            django.urls.reverse('polls:results', args=(question_id,))
+        )
