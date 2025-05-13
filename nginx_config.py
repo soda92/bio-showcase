f915c7a49b07ed7403c21fc05136e609211078c7
Maple <wjxa20152015@gmail.com>
2025-05-13 23:29:19 +0800

    add static collection before starting nginx

--- nginx_config.py
+++ nginx_config.py
@@ -4,7 +4,18 @@ cwd = Path(__file__).resolve().parent
 nginx_config = cwd / 'bio_showcase' / 'nginx.conf'
 
 
+def collectstatic():
+    import subprocess
+    import sys
+
+    subprocess.run(
+        [sys.executable, 'manage.py', 'collectstatic', '--force-color', '--noinput'],
+        check=True,
+    )
+
+
 def get_config() -> str:
+    collectstatic()
     bio_static = cwd / 'bio_static'
     bio_media = cwd / 'bio_media'
     content = nginx_config.read_text(encoding='utf8')
