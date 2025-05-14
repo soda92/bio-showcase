47f3c986fa5fdeefc8a893d224b5ea9781eb45fb
Maple <wjxa20152015@gmail.com>
2025-05-14 19:57:10 +0800

    fix favicon

--- bio_showcase/settings.py
+++ bio_showcase/settings.py
@@ -12,7 +12,7 @@ https://docs.djangoproject.com/en/5.2/ref/settings/
 
 import os
 from sodatools import Path, str_path
-import platform
+import platform  # noqa: F401
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent
