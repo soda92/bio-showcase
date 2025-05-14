9b20396c903543d75394c396478a747ca826e469
Maple <wjxa20152015@gmail.com>
2025-05-14 16:25:02 +0800

    move db to bio_showcase

--- bio_showcase/settings.py
+++ bio_showcase/settings.py
@@ -15,7 +15,7 @@ from sodatools import Path, str_path
 import platform
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
-BASE_DIR = Path(__file__).resolve().parent.parent
+BASE_DIR = Path(__file__).resolve().parent
 
 
 # Quick-start development settings - unsuitable for production
