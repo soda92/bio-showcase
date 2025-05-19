af72a166fe62447761eeb08d3a4d1f39a2ead21b
Maple <wjxa20152015@gmail.com>
2025-05-20 07:28:25 +0800

    allow cors headers from deno

--- bio_showcase/settings.py
+++ bio_showcase/settings.py
@@ -64,6 +64,7 @@ MIDDLEWARE = [
 CORS_ALLOWED_ORIGINS = [
     'http://127.0.0.1:8000',
     'http://localhost:8000',
+    'https://soda-bio-showcase.deno.dev',
 ]
 
 ROOT_URLCONF = 'bio_showcase.urls'
